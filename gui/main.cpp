ae70ae96183ec9f2e309b3f581b852ccfe6256f2
Sergey Volodin <etoestja@yandex.ru>
2012-12-04 15:07:22 +0400

    new gui main

--- gui/main.cpp
+++ gui/main.cpp
@@ -3,6 +3,8 @@
 #include <vect.h>
 #include <math.h>
 
+const number_t eps = 1E-2;
+
 number_t f1_a(number_t x, number_t y)
 {
     number_t res;
@@ -20,53 +22,111 @@ number_t f1_a(number_t x, number_t y)
     return(res);
 }
 
-const number_t eps = 1E-2;
-
-f_result f1(number_t x, number_t y)
+number_t f2_a(number_t x, number_t y)
 {
-    f_result res;
-    res.z = f1_a(x, y);
-    res.color = vect(0.5, 1, 0.1);
-    float d_x = (res.z - f1_a(x + eps, y)) / eps, d_y = (res.z - f1_a(x, y + eps)) / eps;
-    /*res.color.z = sqrt(pow(d_x, 2) + pow(d_y, 2));
-    res.color.y = fabs(d_x) / 100;
-    res.color.x = fabs(d_y) / 100;*/
-    res.color = Draw::get_color(vect(x, x - y, y));
-    res.color /= 8;
-    return(res);
+    number_t res;
+
+    number_t r = (x * x + y * y) / 3;
+
+    return(r);
 }
 
 f_result f2(number_t x, number_t y)
 {
     f_result res;
-    res.color = Draw::get_color(vect(x, x + y, y));
+    res.z = f2_a(x, y);
+    res.color = vect(0.5, 1, 0.1);
+    float d_x = (res.z - f1_a(x + eps, y)) / eps, d_y = (res.z - f1_a(x, y + eps)) / eps;
+    res.color = Draw::get_color(vect(x - y, x + y, y - x));
+    return(res);
+}
 
-    res.z = x * x + y * y + 50;
-    res.z /= 10;
+f_result f0(number_t x, number_t y)
+{
+    f_result res;
+    res.z = 5;
+    res.color = vect(0.5, 1, 0.1);
+    float d_x = (res.z - f1_a(x + eps, y)) / eps, d_y = (res.z - f1_a(x, y + eps)) / eps;
+    /*res.color.z = sqrt(pow(d_x, 2) + pow(d_y, 2));
+    res.color.y = fabs(d_x) / 100;
+    res.color.x = fabs(d_y) / 100;*/
+    //res.color = Draw::get_color(vect(x, x - y, y));
+    //res.color /= 8;
+    return(res);
+}
 
-    //if(sqrt(x * x + y * y) > 8) res.z = sqrt(-1);
-    //if(sqrt(x * x + y * y) < 5.5) res.z = sqrt(-1);
+f_result f1(number_t x, number_t y)
+{
+    f_result res;
+    res.z = 0;
+    res.color = vect(0.5, 1, 0.1);
+    float d_x = (res.z - f1_a(x + eps, y)) / eps, d_y = (res.z - f1_a(x, y + eps)) / eps;
+    /*res.color.z = sqrt(pow(d_x, 2) + pow(d_y, 2));
+    res.color.y = fabs(d_x) / 100;
+    res.color.x = fabs(d_y) / 100;*/
+    //res.color = Draw::get_color(vect(x, x - y, y));
+    //res.color /= 8;
+    return(res);
+}
+
+f_result hyp(number_t x, number_t y)
+{
+    f_result res;
+
+    number_t a = 1, b = 10;
+
+    //res.z = 0.5 * (pow(x / a, 2) - pow(y / b, 2));
+    //res.z = 0.5 * (pow(x / a, 2) + pow(y / b, 2));
+    //res.z = pow(pow(x, 2) - pow(y, 2), 0.5);
+    res.z = 0.5 * (pow(x, 2) - pow(y, 2));
+
+    float d_x = (res.z - f1_a(x + eps, y)) / eps, d_y = (res.z - f1_a(x, y + eps)) / eps;
+    res.color = Draw::get_color(vect(d_x, res.z, d_y));
 
     return(res);
 }
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Draw w;
 
+    w.add_function(hyp);
     w.add_function(f1);
+    w.add_function(f0);
 
-    w.set_gravity(vect(0, -0.5, 0));
-    w.set_use_gravity_n2(true);
+
+//    w.add_function(f1);
+
+    //w.set_gravity(vect(0, -0.5, 0));
+    //w.firework(vect(30, 20, 0), 5, 1, vect(0, 0, 0), 1, 100);
+//    w.set_use_gravity_n2(true);
+//    w.set_use_gravitomagnetism_force(false);
+//    w.set_use_gravitomagnetism_torque(false);
+
+//    w.add_point(vect(0, 10, 0), vect(0, 0, 0), vect(1, 1, 1), 1E13, vect(0, 0, 0), 0);
+//    w.add_point(vect(30, 5, 0), vect(0, 0, 4.6843837106667205), vect(1, 0, 1), 1E7, vect(0, 0, 0), 0);
+//    w.add_point(vect(50, 10, 0), vect(0, 0, 3.6534476867747814), vect(1, 0, 1), 1E7, vect(0, 0, 0), 0);
+//    w.add_point(vect(70, 20, 0), vect(0, 0, 3.0721710631258894), vect(1, 0, 1), 1E10, vect(0, 0, 0), 0);
+//    w.add_point(vect(71, 20, 0), vect(0, 0, 3.0721710631258894 - 0.816935737986777), vect(1, 0, 1), 1, vect(0, 0, 0), 0);
+
+//    w.add_point(vect(-50, 3, -5), vect(1, 0, 0), vect(1, 1, 1), 1E12, vect(0, 0, 0), 1);
+//    w.add_point(vect(50, 3, 5), vect(-1, 0, 0), vect(1, 1, 1), 1E12, vect(0, 0, 0), 1);
+
+    /*w.set_gravity(vect(0, -0.0, 0));
+    w.set_use_gravity_n2(false);
+    w.set_use_gravitomagnetism_force(true);
+    w.set_use_gravitomagnetism_torque(true);
+    w.add_point(vect(0, 5, 0), vect(0, 0, 0), vect(1, 1, 0), 10, vect(0, 10, 0), 1E29);
+    w.add_point(vect(0, 10, 0), vect(1, 0, 0), vect(1, 1, 0), 10, vect(0, 0, 0), 1);*/
+
+    /*w.set_gravity(vect(0, -0.5, 0));
+    w.set_use_gravity_n2(false);
     w.set_use_gravitomagnetism_force(false);
     w.set_use_gravitomagnetism_torque(false);
-
-    w.add_point(vect(0, 10, 0), vect(0, 0, 0), vect(1, 1, 1), 1E13, vect(0, 0, 0), 0);
-    w.add_point(vect(30, 5, 0), vect(0, 0, 4.6843837106667205), vect(1, 0, 1), 1E7, vect(0, 0, 0), 0);
-    w.add_point(vect(50, 10, 0), vect(0, 0, 3.6534476867747814), vect(1, 0, 1), 1E7, vect(0, 0, 0), 0);
-    w.add_point(vect(70, 20, 0), vect(0, 0, 3.0721710631258894), vect(1, 0, 1), 1E10, vect(0, 0, 0), 0);
-    w.add_point(vect(71, 20, 0), vect(0, 0, 3.0721710631258894 - 0.816935737986777), vect(1, 0, 1), 1, vect(0, 0, 0), 0);
+    w.add_points(f2, 5, 0.09, vect(0, 0, 0), 1, vect(0, 0, 0), 1);
+    w.add_function(f1);*/
 
     w.ftl();
     w.show();
