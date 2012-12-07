768cfe891d1a89059579941a8ec0de5cf63a76b5
Sergey Volodin <etoestja@yandex.ru>
2012-12-07 14:59:11 +0400

    ellipsoid

--- DzhanibekovEffect/main.cpp
+++ DzhanibekovEffect/main.cpp
@@ -3,37 +3,42 @@
 #include <vect.h>
 #include <math.h>
 
-number_t a = 2, b = 10, c = 10;
+number_t a = 3, b = 5, c = 7, r = 4;
 
-f_result ellipsoid0(number_t x, number_t y)
+f_result ellipsoid(number_t x, number_t y)
 {
     f_result res;
-    res.color = vect(1, 1, 1);
-    res.coordinates.z = c * sqrt(1 - pow(x / a, 2) - pow(y / b, 2));
-    return(res);
-}
-
-f_result ellipsoid1(number_t x, number_t y)
-{
-    f_result res = ellipsoid0(x, y);
-    res.coordinates.z = -res.coordinates.z;
-    return(res);
-}
-
-f_result pellipsoid(number_t x, number_t y)
-{
-    f_result res;
-    res.color = vect(1, 1, 1);
+    res.color = vect(0, 0.7, 0);
     res.coordinates.x = a * cos(x) * cos(y);
     res.coordinates.y = b * cos(x) * sin(y);
     res.coordinates.z = c * sin(x);
     return(res);
 }
 
-f_result fplane(number_t x, number_t y)
+f_result sphere(number_t x, number_t y)
 {
     f_result res;
-    res.color = vect(0.8, 0, 0);
+    res.color = vect(0.3, 0, 0);
+    res.coordinates.x = r * cos(x) * cos(y);
+    res.coordinates.y = r * cos(x) * sin(y);
+    res.coordinates.z = r * sin(x);
+    return(res);
+}
+
+f_result curve(number_t x, number_t y)
+{
+    f_result res;
+    res.color = vect(0.3, 0, 0);
+    res.coordinates.x = sin(x);
+    res.coordinates.y = cos(x);
+    res.coordinates.z = x;
+    return(res);
+}
+
+f_result plane(number_t x, number_t y)
+{
+    f_result res;
+    res.color = vect(0, 0, 0.1);
     res.coordinates.z = 0;
     return(res);
 }
@@ -43,32 +48,41 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Draw w;
 
-    function plane, e1, e2;
-//    e1.first = (void*) ellipsoid0;
-//    e2.first = (void*) ellipsoid1;
+    function p, e, s, c;
 
-    plane.first = (void*) fplane;
-    e1.first = (void*) pellipsoid;
+    p.first = (void*) plane;
+    e.first = (void*) ellipsoid;
+    s.first = (void*) sphere;
+    c.first = (void*) curve;
 
-    plane.second.type = function_info::T_COORDINATE;
-    plane.second.xmin = -20;
-    plane.second.xmax = 20;
-    plane.second.xstep = 0.1;
-    plane.second.ymin = -20;
-    plane.second.ymax = 20;
-    plane.second.ystep = 0.1;
+    p.second.type = function_info::T_COORDINATE;
+    p.second.xmin = -20;
+    p.second.xmax = 20;
+    p.second.xstep = 0.1;
+    p.second.ymin = -20;
+    p.second.ymax = 20;
+    p.second.ystep = 0.1;
 
-    e1.second = plane.second;
-    e1.second.type = function_info::T_PARAMETRIC;
-    e1.second.xmin = -M_PI / 2;
-    e1.second.xmax =  M_PI / 2;
-    e1.second.ymin = -M_PI;
-    e1.second.ymax =  M_PI;
+    e.second = p.second;
+    e.second.type = function_info::T_PARAMETRIC;
+    e.second.xmin = -M_PI / 2;
+    e.second.xmax =  M_PI / 2;
+    e.second.ymin = -M_PI;
+    e.second.ymax =  M_PI;
 
+    s.second = e.second;
+    c.second = e.second;
+    c.second.xmin = -50;
+    c.second.xmax = 50;
+    c.second.xstep = 0.1;
+    c.second.ymin = 0;
+    c.second.ymax = 0;
+    c.second.ystep = 1;
 
-    w.add_function(e1);
-    w.add_function(plane);
-    //w.add_function(e2);
+    w.add_function(p);
+    //w.add_function(e);
+    //w.add_function(s);
+    w.add_function(c);
 
     w.ftl();
     w.show();
